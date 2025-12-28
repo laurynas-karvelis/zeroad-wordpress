1452acd4830a2e80f4243d9d1a89b492beeb4c25
Laurynas Karvelis <laurynas.karvelis@gmail.com>
2025-12-28 20:43:26 +0200

    Sync package version across different files and places; Package zip file with correct version

---
+++ version-sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=$(jq -r '.version' package.json)
+
+sed -i '' "s/^Stable tag:.*/Stable tag: $VERSION/" plugins/zero-ad-network/readme.txt
+sed -i '' "s/^[[:space:]]*\\*[[:space:]]*Version:.*/ * Version:           $VERSION/" plugins/zero-ad-network/zero-ad-network.php
+sed -i '' "s/define(\"ZERO_AD_NETWORK_PLUGIN_VERSION\", \".*\");/define(\"ZERO_AD_NETWORK_PLUGIN_VERSION\", \"$VERSION\");/" plugins/zero-ad-network/zero-ad-network.php
